fc8d3989756288a66213244be1122aa5ad6e9783
Camilly30 <100858216+Camilly30@users.noreply.github.com>
2023-12-04 22:03:39 -0400

    Commit 07

--- js/db.js
+++ js/db.js
@@ -1,91 +1,99 @@
 import { openDB } from "idb";
 
 let db;
-async function criarDB(){
+
+async function criarDB() {
     try {
         db = await openDB('banco', 1, {
-            upgrade(db, oldVersion, newVersion, transaction){
-                switch  (oldVersion) {
+            upgrade(db, oldVersion, newVersion, transaction) {
+                switch (oldVersion) {
                     case 0:
                     case 1:
                         const store = db.createObjectStore('imob', {
                             keyPath: 'titulo'
                         });
                         store.createIndex('id', 'titulo');
-                        console.log("banco de dados criado!");
-                        
+                        console.log("Banco de dados criado!");
+                        break;
                 }
             }
         });
-        console.log("banco de dados aberto!");
-    }catch (e) {
+        console.log("Banco de dados aberto!");
+    } catch (e) {
         console.log('Erro ao criar/abrir banco: ' + e.message);
     }
 }
-//createDB é carregado assim que o DOM terminar de carregar
-window.addEventListener('DOMContentLoaded', async event =>{
-   await criarDB();
-   document.getElementById('btnCarregar').addEventListener('click', buscarImob);
-    document.querySelectorAll('.btnAdd').forEach(button => {
-        button.addEventListener('click', addImob);
-      });
 
+//createDB é carregado assim que o DOM terminar de carregar
+window.addEventListener('DOMContentLoaded', async event => {
+    await criarDB();
+    document.getElementById('btnCadastrar').addEventListener('click', cadastrarImovel);
+    document.getElementById('btnCarregar').addEventListener('click', buscarImob);
 });
 
-async function buscarImob(){
-    if(db == undefined){
+async function buscarImob() {
+    if (db == undefined) {
         console.log("O banco de dados está fechado.");
     }
     const tx = await db.transaction('imob', 'readonly');
     const store = await tx.objectStore('imob');
-    const produto = await store.getAll();
-    if(produto){
-        const divLista = produto.map(imob => {
+    const imobil = await store.getAll();
+    if (imobil) {
+        const divLista = imobil.map(imob => {
             return `<div class="imob">
-            <p><h1>Seus Interesses</h1></p>
-            <p>Nome do Local - ${imob.titulo}</p>
-            <p>Área - ${imob.descricao}</p>
-            <p>Custo - ${imob.custo}</p>
-           </div>`
+                <h1 style="text-align: center; font-weight: 900;">Novos Imóveis</h1>
+                <p>${imob.tipoImovel}</p>
+                <p>Nome do Local - ${imob.titulo}</p>
+                <p>Descrição - ${imob.descricao}</p>
+                <p>Custo R$ -${imob.custo}</p>
+                <p>Latitude:${imob.latitude}</p>
+                <p>Longitude:${imob.longitude}</p>
+                <div class="mapinha">
+                <div class="gmap_canvas">
+                    <iframe width="600" height="500" id="gmap_canvas_${imob.titulo}" 
+                        src="https://maps.google.com/maps?q=${imob.latitude}%2C${imob.longitude}&t=&z=13&ie=UTF8&iwloc=&output=embed" 
+                        frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                    <a href="https://123movies-i.net"></a>
+                    <style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style>
+                                <a href="https://www.embedgooglemap.net">google maps embed</a>
+                                <style>.gmap_canvas{overflow:hidden;background:none!important;height:500px;width:600px;}</style>
+                    <br/>
+                </div>
+            </div>
+            </div>`;
         });
         listagem(divLista.join(' '));
     }
 }
-async function addImob(event) {
+
+async function cadastrarImovel() {
     console.log('it´s working');
-    const button = event.target;
-    const itemDiv = button.closest('.item');
 
-    let tipoImovel = button.getAttribute('data-type');
-    let titulo = itemDiv.querySelector("#titulo").textContent;
-    let descricao = itemDiv.querySelector("#descricao").textContent;
-    let custo = itemDiv.querySelector("#custo").textContent;
+    const titulo = document.getElementById("titulo").value;
+    const descricao = document.getElementById("descricao").value;
+    const custo = document.getElementById("custo").value;
+    const tipoImovel = document.getElementById("tipoImovel").value;
 
-    const tx = db.transaction('imob', 'readwrite');
+    let latitude = document.getElementById("latitude").value;
+    let longitude = document.getElementById("longitude").value;
+
+    const tx = await db.transaction('imob', 'readwrite');
     const store = tx.objectStore('imob');
+    
 
-    // Verificar se a chave (titulo) já existe no objeto de armazenamento
-    const existingItem = await store.get(titulo);
-
-    if (existingItem) {
-        console.log('Registro com a chave já existe:', existingItem);
-        alert('Este item já está na sua lista de interesses!');
-    } else {
-        // A chave não existe, adicione o novo registro
-        try {
-            await store.add({ tipoImovel, titulo, descricao, custo });
-            console.log('Registro adicionado com sucesso:', { tipoImovel, titulo, descricao, custo });
-            alert('O item foi adicionado na sua lista de interesses!');
-        } catch (error) {
-            console.error('Erro ao adicionar registro:', error);
-            tx.abort();
-        }
+    try {
+        await store.add({ titulo:titulo, descricao:descricao, custo:custo, tipoImovel:tipoImovel , latitude: latitude, longitude:longitude});
+        console.log('Registro adicionado com sucesso:', { titulo:titulo, descricao:descricao, custo:custo, tipoImovel:tipoImovel, latitude: latitude, longitude:longitude});
+        alert('Registro adicionado com sucesso!!');
+    } catch (error) {
+        console.log('Erro ao adicionar registro:', error);
+        tx.abort();
     }
 
     await tx.done;
+    await buscarImob();
 }
 
-
-function listagem(text){
+function listagem(text) {
     document.getElementById('resultados').innerHTML = text;
 }
